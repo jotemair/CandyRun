47b067cf4bdcb0a62962b6deb8a109f66d5d73fa
Helmi Amsani <48036579+helmiamsani@users.noreply.github.com>
2019-09-11 15:37:41 +1000

    Add comment to wind Area script

--- Assets/Scripts/Effects/WindArea.cs
+++ Assets/Scripts/Effects/WindArea.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class WindArea : MonoBehaviour
 {
-    public float strength = 1000f;
-    public Vector3 direction = new Vector3(0,0,1);
+    public float strength = 1000f; // Strength stores the value of wind speed
+    public Vector3 direction = new Vector3(0,0,1); // in which direction does the wind push?, default is on the z value
 
-    private Rigidbody _rb;
-    private GameObject _player;
+    private Rigidbody _rb; // _rb stores player rigidbody
+    private GameObject _player; //_player stores player game object
 
     private void Start()
     {
-        _player = GameObject.Find("Sphere");
-        _rb = _player.GetComponent<Rigidbody>();
+        _player = GameObject.Find("Sphere"); // Get player reference
+        _rb = _player.GetComponent<Rigidbody>(); // Get player rigidbody reference
     }
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerStay(Collider other) // when something is inside the wind area
     {
-        if (_player)
+        if (_player) // if it is player
         {
-            _rb.AddForce(direction * strength);
+            _rb.AddForce(direction * strength); // Add strength and direction to rigidbody. 
         }
     }
 }
