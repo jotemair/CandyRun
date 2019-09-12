e05f05b384e82ba100d33a9ba68db331278cf736
helmiamsani <48036579+helmiamsani@users.noreply.github.com>
2019-09-12 20:22:05 +1000

    yeet

--- Assets/Scripts/Effects/WindArea.cs
+++ Assets/Scripts/Effects/WindArea.cs
@@ -6,4 +6,12 @@ public class WindArea : MonoBehaviour
 {
     public float strength = 500f; // Strength stores the value of wind speed
     public Vector3 direction = new Vector3(0,0,1); // in which direction does the wind push?, default is on the z value
+
+    private Rigidbody _rigid;
+
+    private void OnTriggerStay(Collider other)
+    {
+        _rigid = other.attachedRigidbody;
+        _rigid.AddForce(direction * strength);
+    }
 }
