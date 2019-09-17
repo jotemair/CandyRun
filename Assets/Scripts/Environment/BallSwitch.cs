d96182c1e017cfd70643c2eef42d73bb21f69faa
Adam Rudas <jotemair@gmail.com>
2019-09-17 11:32:52 +1000

    Ball prefabs and change areas

---
+++ Assets/Scripts/Environment/BallSwitch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSwitch : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _ballPrefab = null;
+
+    private bool _active = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_active && (other.CompareTag("Player")))
+        {
+            Destroy(other.gameObject);
+            Spawn();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _active = true;
+        }
+    }
+
+    public void Spawn()
+    {
+        Instantiate<GameObject>(_ballPrefab, transform.position, Quaternion.identity);
+        _active = false;
+    }
+}
