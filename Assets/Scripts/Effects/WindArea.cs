9bbbd8a713a435527b19b7fa430ac63e9f5f3419
Adam Rudas <jotemair@gmail.com>
2019-09-19 17:19:06 +1000

    Added Gumball machine and track model prefabs

--- Assets/Scripts/Effects/WindArea.cs
+++ Assets/Scripts/Effects/WindArea.cs
@@ -7,11 +7,13 @@ public class WindArea : MonoBehaviour
     public float strength = 500f; // Strength stores the value of wind speed
     public Vector3 direction = new Vector3(0,0,1); // in which direction does the wind push?, default is on the z value
 
+    public bool worldSpace = true; // Is the direction given in world space or local space
+
     private Rigidbody _rigid;
 
     private void OnTriggerStay(Collider other)
     {
         _rigid = other.attachedRigidbody;
-        _rigid.AddForce(direction * strength);
+        _rigid.AddForce((worldSpace ? direction : transform.TransformDirection(direction)) * strength);
     }
 }
