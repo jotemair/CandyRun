a818a5aeabd9086a37314ec7bb2956d38b10e5f7
Adam Rudas <jotemair@gmail.com>
2019-09-25 16:14:56 +1000

    Added button and moving parts

---
+++ Assets/Scripts/Environment/MovingPiece.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPiece : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _movingPiece = null;
+
+    [SerializeField]
+    private float _moveSpeed = 0.1f;
+
+    [SerializeField]
+    private float _rotationSpeed = 0.1f;
+
+    [SerializeField]
+    private List<Transform> _positions = new List<Transform>();
+
+    [SerializeField]
+    private bool _loop = true;
+
+    [SerializeField]
+    private bool _active = true;
+
+    [SerializeField]
+    private bool _reverse = false;
+
+    [SerializeField]
+    private int _targetPosIdx = 0;
+
+    private Vector3 _velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (0 == _positions.Count)
+        {
+            enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_active)
+        {
+            _movingPiece.position = Vector3.SmoothDamp(_movingPiece.position, _positions[_targetPosIdx].position, ref _velocity, Time.deltaTime, _moveSpeed);
+            _movingPiece.rotation = Quaternion.RotateTowards(_movingPiece.rotation, _positions[_targetPosIdx].rotation, _rotationSpeed);
+
+            if ((_movingPiece.position.Equals(_positions[_targetPosIdx].position)) && (_movingPiece.rotation.Equals(_positions[_targetPosIdx].rotation)))
+            {
+                StepTargetIdx();
+            }
+        }
+    }
+
+    private void StepTargetIdx()
+    {
+        _targetPosIdx = ((_loop) ? ((_targetPosIdx + (_reverse ? -1 : 1)) % _positions.Count) : Mathf.Clamp((_targetPosIdx + (_reverse ? -1 : 1)), 0, _positions.Count - 1));
+    }
+
+    public void On()
+    {
+        _active = true;
+    }
+
+    public void Off()
+    {
+        _active = false;
+    }
+
+    public void Toggle()
+    {
+        _active = !_active;
+    }
+
+    public void Reverse()
+    {
+        _reverse = !_reverse;
+        StepTargetIdx();
+    }
+}
---
+++ Assets/Scripts/Environment/PhysicsButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PhysicsButton : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _button = null;
+
+    [SerializeField]
+    private bool _oneTime = true;
+
+    private bool _activated = false;
+
+    public UnityEvent onTrigger;
+    public UnityEvent offTrigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_activated && (_button.localPosition.y < 0f))
+        {
+            if (_oneTime)
+            {
+                _button.GetComponent<Rigidbody>().isKinematic = true;
+            }
+
+            onTrigger.Invoke();
+            _activated = true;
+        }
+
+        if ((_activated) && (_button.localPosition.y > 0.085f))
+        {
+            offTrigger.Invoke();
+            _activated = false;
+        }
+    }
+}
