be1f4f01bf26a43993961198c47c72b2b98be1ab
tristanb3d <49214738+tristanb3d@users.noreply.github.com>
2019-10-06 23:24:51 +1100

    floorupD
    floor update,

---
+++ Assets/Scripts/things_todo_things/box_destroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class box_destroy : MonoBehaviour
+{
+    public GameObject boxodeado;
+   
+    public void  destro()
+    
+        {
+        // Destroy(boxodeado);
+        //  Destroy(boxodeadoClone);
+       // Destroy(GameObject.FindWithTag("boxo"));
+        // Instantiate(BoxoObjectO, new Vector3(0, 55, 272), Quaternion.identity);
+    }
+        //    Instantiate(Boxo, new Vector3(2.0F, 0, 0), Quaternion.identity);
+    
+}
+
---
+++ Assets/Scripts/things_todo_things/box_reset.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class box_reset : MonoBehaviour
+{
+    //  public Transform Boxo;
+    public new GameObject BoxoObjectO;
+    public void Reseto_Spawno_yolo()
+    {
+        for (int i = 1; i != 2; i++)
+        {
+            Destroy(BoxoObjectO);
+            Instantiate(BoxoObjectO, new Vector3(0, 55, 272), Quaternion.identity);
+        }
+        //    Instantiate(Boxo, new Vector3(2.0F, 0, 0), Quaternion.identity);
+    }
+
+    public void EFG()
+    {
+        Destroy(GameObject.FindWithTag("boxo"));
+    }
+
+}
